50d164af06dc18082fab1fcf2576d46c4fb4f317
Jim Watkins <james.watkins77@yahoo.com>
2017-02-16 10:19:07 -0700

    added about us and landing components

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import Landing from './components/landing';
+import AboutUs from './components/about_us';
 
 export default (
-    <Route path='/' component={App} />
+    <Route path='/' component={App}>
+        <IndexRoute component={Landing} />
+        <Route path='about' component={AboutUs} />
+    </Route>
 );
