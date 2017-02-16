bb4b9e3047b2882a4ceea8fb0e8c28fce5c89b2c
Jim Watkins <james.watkins77@yahoo.com>
2017-02-16 14:21:29 -0700

    created header component

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+          <nav className="navbar navbar-default wood">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">
+                    <img className='logo' alt="Howards" src="src/images/HowardsLogo.jpg" />
+                </a>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li><a href="#">About Us</a></li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                    <li><a href="#">Our Services</a></li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                    <li><a href="#">Gallery</a></li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+      </div>
+    );
+  }
+}
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>Landing Page</div>
+      <div>
+          <Header />
+      </div>
     );
   }
 }
